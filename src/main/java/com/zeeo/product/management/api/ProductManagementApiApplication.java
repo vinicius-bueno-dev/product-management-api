2cfa14c2909002d234a3ed0740af35d3df1f9409
Vinicius Bueno <buenovinicius1999@gmail.com>
2024-05-20 20:36:59 -0300

    new project

---
+++ src/main/java/com/zeeo/product/management/api/ProductManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.zeeo.product.management.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementApiApplication.class, args);
+	}
+
+}
